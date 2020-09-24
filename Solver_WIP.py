4354e328362e35140f0a2aea691c8a5ccffbd359
Jan-Ruben Schmid <jschm20@student.sdu.dk>
2020-09-24 19:24:58 +0200

    Need's to be debugged

--- Solver_WIP.py
+++ Solver_WIP.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 import hashlib
 
 
@@ -18,32 +19,115 @@ def parse(map = 'Sokoban_map.txt'):
     for ch in line:
       temp.append(ch)
     map_ls.append(temp)
+
   return map_ls
+  
 
 
+def FindSokobanOnMap(map):
+  for i in range(len(map)):
+    for k in range(len(map[0])):
+      if(map[i][k] == '@' or map[i][k] == '+'):
+        return {"row": i, "column":k}
+
+def PositionOnMap(map, position):
+  pos= map[int(position["row"])][int(position["column"])]
+  return pos
+
+def SetPositionOnMap(map, position, positionValue):
+  map[int(position["row"])][int(position["column"])] = positionValue
+
+def WalkSokoban(map, oldPosition, newPosition, newPositionValue):
+  map[int(newPosition["row"])][int(newPosition["column"])] = newPositionValue
+  if(PositionOnMap(map, oldPosition) == '+'):
+    SetPositionOnMap(map, oldPosition, '.')
+  else:
+    SetPositionOnMap(map, oldPosition, ' ')
+
+def MoveBox(map, boxPosition, direction):
+  nextPosition = copy.copy(boxPosition)
+  
+  if(direction=="down"): 
+    nextPosition["row"] = boxPosition["row"]+1
+  elif(direction=="up"): 
+    nextPosition["row"] = boxPosition["row"]-1
+  elif(direction=="left"): 
+    nextPosition["column"] = boxPosition["column"]-1
+  elif(direction=="right"): 
+    nextPosition["column"] = boxPosition["column"]+1
+  else:
+    raise Exception("Sorry, i don't know where you want to go: {0}".format(direction))
 
 
-#
-# def move_sokoban(map, direction):
-#  switch (){
-#  case 1: wall
-#  case 2: can 
-#  case 3: empty
-#  case 4: goal
-#  case 5: can_on_goal
+  if(int(nextPosition["column"])<0 or int(nextPosition["row"])<0 or 
+    int(nextPosition["column"]) > len(map) or int(nextPosition["column"])> len(map[0])):
+    return False
+  
+  #free, let's go
+  if(PositionOnMap(map, nextPosition) == ' '):
+    SetPositionOnMap(map, nextPosition, '$')
+  elif(PositionOnMap(map, nextPosition) == '.'):
+    SetPositionOnMap(map, nextPosition, '*')
+  else:
+    return False
+  return True
+    
 
-#  }
-#  check hash
+def MoveSokoban(map, direction):
+  currentPosition = FindSokobanOnMap(map)
+  nextPosition = copy.copy(currentPosition)
 
-def hashList(ls, Hash_ls):
+  if(direction=="down"): 
+    nextPosition["row"] = currentPosition["row"]+1
+  elif(direction=="up"): 
+    nextPosition["row"] = currentPosition["row"]-1
+  elif(direction=="left"): 
+    nextPosition["column"] = currentPosition["column"]-1
+  elif(direction=="right"): 
+    nextPosition["column"] = currentPosition["column"]+1
+  else:
+    raise Exception("Sorry, i don't know where you want to go: {0}".format(direction))
+
+  #check if position is valid
+  if(int(nextPosition["column"])<0 or int(nextPosition["row"])<0 or 
+    int(nextPosition["column"]) > len(map) or int(nextPosition["column"])> len(map[0])):
+    return None
+  
+  if(PositionOnMap(map, nextPosition) == ' '):
+    #free spot, let's go
+    WalkSokoban(map, currentPosition, nextPosition, '@')
+  elif(PositionOnMap(map, nextPosition) == '#'):
+    #wall, we can't go here, so here is a dead end...
+    return None
+  elif(PositionOnMap(map,nextPosition) == '$'):
+    #let's move the box
+    if(MoveBox(map, nextPosition, direction)):
+      WalkSokoban(map, currentPosition, nextPosition, '@')
+    else:
+      return None
+  elif(PositionOnMap(map, nextPosition) == '.'):
+    WalkSokoban(map, currentPosition, nextPosition,'+')
+  elif(PositionOnMap(map, nextPosition) == '*'):
+    if(MoveBox(map, nextPosition, direction)):
+      WalkSokoban(map, currentPosition, nextPosition, '+')
+    else:
+      return None
+  return copy.deepcopy(map)
+
+def hashList(Hash_ls,ls):
+  if(Hash_ls==None):
+    return True
+
   s = ' '.join([str(elem) for elem in ls]) 
   new_hash = hashlib.sha256(s.encode())
   new_hash = new_hash.hexdigest()
 
   if (ismember(Hash_ls,new_hash)):
-    Hash_ls.append(new_hash)
-  
-  return Hash_ls
+    ls.append(new_hash)
+    return False
+  return True
+
+
 
 
 def ismember(ls, num):
@@ -51,12 +135,48 @@ def ismember(ls, num):
       if (i == num):
         return 0
     return 1
-    
+  
+def CheckIfSolved(map):
+  for i in range(len(map)):
+    for k in range(len(map[i])):
+      if(map[i][k] == '$'):
+        return False
+  return True
 
 def main():
     prev_states = []
+    directions = ["up", "down", "left", "right"]
     map = parse()
-    prev_states = hashList(map, prev_states)
+    hashList(map, prev_states)
+    openList=[]
+    openList.append(MoveSokoban(map, "up"))
+    count = 0
+    while(count< len(openList)):
+      for d in directions:
+        newMap = MoveSokoban(openList[count], d)
+       
+        if(hashList(newMap, prev_states) == False):
+          CheckIfSolved(newMap)
+          openList.append(newMap)
+      # newMapDown = MoveSokoban(openList[count], "down")
+      # newMapLeft = MoveSokoban(openList[count], "left")
+      # newMapRight = MoveSokoban(openList[count], "right")
+      
+      
+      # if(hashList(newMapUp, prev_states) == False):
+      #   openList.append(newMapUp)
+
+      # if(hashList(newMapDown, prev_states) == False):
+      #   openList.append(newMapDown)
+
+      # if(hashList(newMapLeft, prev_states) == False):
+      #   openList.append(newMapLeft)
+
+      # if(hashList(newMapRight, prev_states) == False):
+      #   openList.append(newMapRight)  
+      count+=1
+
+
 
 
 if __name__ == "__main__":
